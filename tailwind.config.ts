da1b9d70abbb396a52b5fa323a1052e111a8d6e4
AndyGuit <totalslash@msn.com>
2024-02-15 03:52:33 +0200

    feat: add tailwind setup and util classes for typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,37 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        sm: '375px',
+        md: '768px',
+        lg: '1440px',
+        xl: '1920px',
+      },
+      container: {
+        center: true,
+        screens: {
+          sm: '343px',
+          md: '688px',
+          lg: '1280px',
+          xl: '1520px',
+        },
+      },
+      colors: {
+        white: '#FEFEFE',
+        'grey-bg-1': '#F6F6F6',
+        'grey-bg-2': '#FBFBFB',
+        'grey-stroke': '#D6D6D6',
+        'grey-inactive': '#B3B3B3',
+        primary: '#FFB52E',
+        secondary: '#0058F6',
+        teritary: '#282828',
       },
     },
   },
