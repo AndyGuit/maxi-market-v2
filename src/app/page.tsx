043de638bb0b908f1e50b0aa2df26f48fed1b553
AndyGuit <totalslash@msn.com>
2024-02-16 08:14:18 +0200

    feat: add logo icon and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { IconAccount } from '@/components/Icons/Icons';
+import { IconAccount, IconMainLogo } from '@/components/Icons/Icons';
 import Button from '@/components/UI/Button';
 
 export default function Home() {
   return (
     <div className="bg-secondary">
       <h1 className="header-1">maxi market</h1>
+      <IconMainLogo />
       <Button type="button" style="wide-secondary">
         Увійти
       </Button>
