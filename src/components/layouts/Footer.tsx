70b6d0c6376333d6c7de1f9bcb1449ed7205e991
AndyGuit <totalslash@msn.com>
2024-02-18 10:14:56 +0200

    feat: add responsive styles to footer

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -60,14 +60,16 @@ const NAVBARS: NavBar[] = [
 
 export default function Footer() {
   return (
-    <footer className="bg-tertiary body-1 px-4 py-10 flex flex-col items-center gap-5">
-      <div className="w-[100px] h-12">
-        <IconMainLogo />
+    <footer className="bg-tertiary body-1 px-4 py-10 flex flex-col items-center gap-5 md:gap-10 md:py-16 md:px-10 xl:flex-row xl:justify-between xl:p-20 2xl:px-[200px] 2xl:pt-[60px] 2xl:pb-[180px]">
+      <div className="flex flex-col gap-5 items-center md:gap-10 lg:items-start xl:gap-16">
+        <div className="w-[100px] h-12">
+          <IconMainLogo />
+        </div>
+        <NavSocial />
       </div>
-      <NavSocial />
-      <div className="w-full flex flex-col gap-2">
-        {NAVBARS.map(nav => (
-          <NavLinks {...nav} />
+      <div className="w-full flex flex-col gap-2 md:flex-row md:justify-between lg:max-w-[945px]">
+        {NAVBARS.map((nav, i) => (
+          <NavLinks key={i} {...nav} />
         ))}
       </div>
     </footer>
